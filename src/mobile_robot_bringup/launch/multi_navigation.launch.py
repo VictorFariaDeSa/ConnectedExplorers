ade50ce61930bd83ad07664980aefe7461ad7fa3
VictorFariadeSa <Savictor3963@gmail.com>
2025-11-16 17:58:30 -0300

    feat: map in rviz

--- src/mobile_robot_bringup/launch/multi_navigation.launch.py
+++ src/mobile_robot_bringup/launch/multi_navigation.launch.py
@@ -1,12 +1,9 @@
 '''
 ********************************************************************************
-* Slam Launch file
-    - This launch file must be used to generate the map
+* Multi navigation Launch file
+    - This launch file is used to generate multiple robots and control them
 ********************************************************************************
 '''
-
-
-
 
 import os
 from launch import LaunchDescription
@@ -21,7 +18,10 @@ from launch.substitutions import Command
 * User defined params
 ********************************************************************************
 '''
-
+robots = [
+    {"name":"robot1","x":"1","y":"-3"},
+    {"name":"robot2","x":"-2","y":"-1"}
+]
 
 '''
 ********************************************************************************
@@ -30,7 +30,6 @@ from launch.substitutions import Command
 '''
 description_pkg = 'mobile_robot_description'
 bringup_pkg = 'mobile_robot_bringup'
-
 
 '''
 ********************************************************************************
@@ -41,25 +40,41 @@ world_path = os.path.join(
     get_package_share_directory(bringup_pkg), 'worlds', 'proj_world.world'
     )
 rviz_config_file = os.path.join(
-    get_package_share_directory(bringup_pkg), 'slam_config', 'rviz_config.rviz'
+    get_package_share_directory(bringup_pkg), 'config', 'rviz_config.rviz'
     )
 urdf_path = os.path.join(
     get_package_share_directory(description_pkg), 'URDF', 'MineMapper.urdf.xacro'
     )
-gazebo_config_path = os.path.join(
-    get_package_share_directory(bringup_pkg), 'slam_config', 'gazebo_bridge.yaml'
-    )
-slam_toolbox_params_file = os.path.join(
-    get_package_share_directory(bringup_pkg), 'slam_config', 'mapper_params_online_async.yaml'
-    )
+
 model_path = os.path.join(
     get_package_share_directory('mobile_robot_bringup'),
     'models'
 )
+gazebo_config_path = os.path.join(
+    get_package_share_directory(bringup_pkg), 'config', 'gazebo_bridge.yaml'
+    )
 
 os.environ['GZ_SIM_RESOURCE_PATH'] = (
     os.environ.get('GZ_SIM_RESOURCE_PATH', '') + ':' + model_path
 )
+
+
+
+nav_config_path = os.path.join(
+    get_package_share_directory(bringup_pkg), 'config', 'nav2_params.yaml'
+)
+
+nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+
+bringup_launch_file = os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
+
+map_file = os.path.join(
+    get_package_share_directory(bringup_pkg), 'maps', 'my_map.yaml'
+)
+
+
+
+
 
 '''
 ********************************************************************************
@@ -69,11 +84,13 @@ os.environ['GZ_SIM_RESOURCE_PATH'] = (
 def generate_launch_description():
     launch_nodes = []
 
+
     '''
     ****************************************************************************
     * GAZEBO
     ****************************************************************************
     '''
+
     launch_gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
@@ -82,14 +99,6 @@ def generate_launch_description():
     )
     launch_nodes.append(launch_gazebo_node)
 
-    gazebo_bridge_node = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        name='ros_gz_bridge',
-        parameters=[{'config_file': gazebo_config_path}],
-        output='screen'
-    )
-    launch_nodes.append(gazebo_bridge_node)
 
     '''
     ****************************************************************************
@@ -105,68 +114,69 @@ def generate_launch_description():
     )
     launch_nodes.append(launch_rviz_node)
 
-
     '''
     ****************************************************************************
-    * Robot creation
+    * MAP SERVER GLOBAL MINIMALISTA (Apenas map e use_sim_time)
     ****************************************************************************
     '''
-
-    state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': Command(['xacro ', urdf_path]),
-            'use_sim_time': True,
-        }]
-    )
-    launch_nodes.append(state_publisher_node)
-
-    robot_spawn_node = Node(
-        package='ros_gz_sim',
-        executable='create',
-        name='spawn_entity',
-        output='screen',
-        arguments=[       
-            '-name', 'MineMapper',                      
-            '-topic', 'robot_description',  
-            '-x', '0',                    
-            '-y', '-3',                    
-            '-z', '0.1'                     
-        ]
-    )
-    launch_nodes.append(robot_spawn_node)
-
-    
-
-
-    '''
-    ****************************************************************************
-    * SLAM
-    ****************************************************************************
-    '''
-    launch_slam_toolbox = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('slam_toolbox'),
-                'launch',
-                'online_sync_launch.py' # Usa o launch file que você testou
-            )
-        ]),
+    launch_global_map_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
+        ),
         launch_arguments={
-            # Passa o caminho completo do seu arquivo de parâmetros para o argumento 'params_file'
-            'params_file': slam_toolbox_params_file, 
-            'use_sim_time': 'true' # Garante a sincronização com o Gazebo
+            'map': map_file,
+            'use_sim_time': "True",
+            'params_file': nav_config_path,
         }.items()
     )
-    launch_nodes.append(launch_slam_toolbox)   
+    launch_nodes.append(launch_global_map_server)
+
+    '''
+    ****************************************************************************
+    * Robots
+    ****************************************************************************
+    '''
+    for robot in robots:
+        name = robot["name"]
+        x_pos = robot["x"]
+        y_pos = robot["y"]
+        group = GroupAction([
+        PushRosNamespace(name),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro ', urdf_path,' robot_ns_arg:=', name]),
+                'use_sim_time': True,
+                'frame_prefix': f'{name}/'
+            }]
+        ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            name='spawn_entity',
+            output='screen',
+            arguments=[
+                '-name', name,                  
+                '-topic', 'robot_description',  
+                '-x', x_pos,                    
+                '-y', y_pos,                    
+                '-z', '0.1'                     
+            ]
+        )
+        ])
+        launch_nodes.append(group)
 
 
-
-
-
+    gazebo_bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='ros_gz_bridge',
+        parameters=[{'config_file': gazebo_config_path}],
+        output='screen'
+    )
+    launch_nodes.append(gazebo_bridge_node)
 
     return LaunchDescription(launch_nodes)
-
